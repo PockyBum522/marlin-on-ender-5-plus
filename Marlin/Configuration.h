12ab34e14d4b16535476f322ac56850f5f8b6d74
DSikes <david@sikespc.com>
2023-04-23 12:58:33 -0400

    Fixed bed xy dims, undersized for now while testing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1706,8 +1706,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 358
-#define Y_BED_SIZE 370
+#define X_BED_SIZE 340
+#define Y_BED_SIZE 340
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS  8
